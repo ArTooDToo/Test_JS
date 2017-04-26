b261dc189da42ad29fe3bdc5b8cc4c7d2f94a549
ArTooDToo <ArTooDToo@users.noreply.github.com>
2017-04-26 12:44:21 +0200

    update...

--- app.js
+++ app.js
@@ -1,13 +1,65 @@
-function DIV() {
-  let el,x,y
-      this.add = function(par, ell, i) {
+
+var bo = document.getElementById("body")
+var a = document.createElement('div')
+  a.setAttribute("id","dziaa")
+  a.style.width = "200px"
+  a.style.height = "200px"
+  a.style.background = "black"
+
+var b = document.createElement('div')
+    b.setAttribute("id","f")
+    b.style.width = "100px"
+    b.style.height = "100px"
+    b.style.background = "blue"
+
+    var c = document.createElement('div')
+        c.setAttribute("id","f")
+        c.style.width = "100px"
+        c.style.height = "100px"
+        c.style.background = "green"
+
+var tab = []
+tab.push(a)
+tab.push(b)
+tab.push(c)
+console.log(tab);
+var i = 0
+function typeWriter() {
+  for (i;i<tab.length;i++) {
+    (function(e){
+      setTimeout(function() {
+        bo.appendChild(tab[e])
+          console.log(e);
+      }, i * 1000);
+    })(i)
+  }
+}
+
+console.log(tab);
+typeWriter()
+/*
+function doSetTimeout(i) {
+  setTimeout(function() { alert(i); }, i* 2000);
+}
+
+for (var i = 1; i <= 2; ++i)
+  doSetTimeout(i);
+
+*/
+
+
+
+
+/*function DIV() {
+  let el
+    this.add = function(par, ell, i) {
       this.ell = document.createElement(ell)
       this.ell.setAttribute("id", i)
       this.par = document.getElementById(par)
       this.par.appendChild(this.ell)
     },
     this.talk = function() {
-        alert( this.name + " say meeow!" )
+        alert("działa")
     },
     this.Id = function (e) {
         this.name = e;
@@ -31,23 +83,24 @@ function DIV() {
         }
     }
 }
-
-
+var tab = []
 var d = new DIV()
 d.add("body", "div", "pierwszy")
 d.Id("pierwszy")
 d.st("black")
 d.xy(200,200)
-
+tab.push(d)
 var f = new DIV()
 f.add("pierwszy", "div", "drugi")
 f.Id("drugi");
 f.st("blue")
 f.xy(100, 100)
-
-
+tab.push(f)
 var g = new DIV()
 g.add("drugi", "div", "trzeci");
 g.Id("trzeci");
 g.st("green")
 g.xy(50, 50)
+tab.push(g)
+console.log(tab);
+*/
